0c5df719b0dc33ecf594ce606f827c072062acd2
Chitranjan kumar <chitranjan75075@gmail.com>
2025-07-29 19:54:53 +0530

    first commit

--- script.js
+++ script.js
@@ -28,4 +28,40 @@ smallimg[2].onclick = function () {
 smallimg[3].onclick = function () {
     MainImg.src = smallimg[3].src;
 }
+// ------------------------------------sprodect1------------------------
+  var MainImg1 = document.getElementById("MainImg1");
+var smallimg1 = document.getElementsByClassName("small-img1");
 
+smallimg1[0].onclick = function () {
+    MainImg1.src = smallimg1[0].src;
+}
+smallimg1[1].onclick = function () {
+    MainImg1.src = smallimg1[1].src;
+}
+smallimg1[2].onclick = function () {
+    MainImg1.src = smallimg1[2].src;
+}
+smallimg1[3].onclick = function () {
+    MainImg1.src = smallimg1[3].src;
+}
+// ------------------------login------------------
+  let isLogin = true;
+
+    function toggleForm() {
+      isLogin = !isLogin;
+      document.getElementById("formTitle").textContent = isLogin ? "Login" : "Sign Up";
+      document.getElementById("nameField").classList.toggle("hidden", isLogin);
+      document.getElementById("submitBtnText").textContent = isLogin ? "Login" : "Sign Up";
+      document.getElementById("toggleText").textContent = isLogin
+        ? "Don’t have an account?"
+        : "Already have an account?";
+      document.getElementById("toggleBtn").textContent = isLogin ? "Sign Up" : "Login";
+    }
+
+    function openModal() {
+      document.getElementById("modal").classList.remove("hidden");
+    }
+
+    function closeModal() {
+      document.getElementById("modal").classList.add("hidden");
+    }
